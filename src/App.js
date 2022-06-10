e539de7a9eeb633fdc55a1a269ffd64a7dccd793
tyoprataama <tyoprataama004@gmail.com>
2022-06-10 22:16:58 +0700

    Build sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./page/navbar/Navbar";
+import Sidebar from "./page/sidebar/Sidebar";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar/>
+    <div className='container'>
+      <Sidebar/>
+      <div className='other'>other</div>
+    </div>
     </div>
   );
 }
--- src/page/navbar/Navbar.jsx
+++ src/page/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NotificationsNone } from '@material-ui/icons';
+import { Language, NotificationsNone, Settings } from '@material-ui/icons';
 import React from 'react';
 import './Navbar.css';
 
@@ -7,12 +7,21 @@ const Navbar = () => {
     <div className='navb'>
       <div className='nav-wrap'>
         <div className='left'>
-          <span className='logo'>admin.tenun</span>
+          <span className='logo'>TenunAdmin</span>
         </div>
         <div className='right'>
           <div className='navb-icon'>
+            <Settings/>
+          </div>
+           <div className='navb-icon'>
+            <Language/>
+            <span className='badge-icon'>3</span>
+          </div>
+           <div className='navb-icon'>
             <NotificationsNone/>
-            </div>
+             <span className='badge-icon'>3</span>
+          </div>
+          <img src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80' alt='' className='user-pict'/>
         </div>
       </div>
     </div>
