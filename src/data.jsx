70ebfb92dacc559972fb67bfe59c2d92ee2ce390
tyoprataama <tyoprataama004@gmail.com>
2022-06-14 12:35:48 +0700

    Build users sections

--- src/data.jsx
+++ src/data.jsx
@@ -47,3 +47,85 @@ export const userdata = [{
         'Active User': 3990,
     },
 ];
+
+export const UserRows = [{
+        id: 1,
+        username: 'Jon Snow',
+        profile: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        email: 'jonsnow@gmail.com',
+        status: 'active',
+        amount: 'Rp 771.500'
+    },
+    {
+        id: 2,
+        username: 'Daenerys Targaryen',
+        profile: 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        email: 'khalesii@gmail.com',
+        status: 'active',
+        amount: 'Rp 1.373.000'
+    },
+    {
+        id: 3,
+        username: 'Arya Stark',
+        profile: 'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        email: 'aryastark@gmail.com',
+        status: 'active',
+        amount: 'Rp 919.500'
+    },
+    {
+        id: 4,
+        username: 'Cersei Lannister',
+        profile: 'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+        email: 'cerseilannister@gmail.com',
+        status: 'active',
+        amount: 'Rp 2.219.200'
+    },
+    {
+        id: 5,
+        username: 'Jaime Lannister',
+        profile: 'https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        email: 'jmlannister@gmail.com',
+        status: 'active',
+        amount: 'Rp 1.620.000'
+    },
+    {
+        id: 6,
+        username: 'Tyrion Lannister',
+        profile: 'https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=846&q=80',
+        email: 'tylannister@gmail.com',
+        status: 'active',
+        amount: 'Rp 812.700'
+    },
+    {
+        id: 7,
+        username: 'Rob Stark',
+        profile: 'https://images.unsplash.com/photo-1539125530496-3ca408f9c2d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        email: 'robstark@gmail.com',
+        status: 'active',
+        amount: 'Rp 1.025.000'
+    },
+    {
+        id: 8,
+        username: 'Jorah Mormont',
+        profile: 'https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        email: 'jomormont@gmail.com',
+        status: 'active',
+        amount: 'Rp 153.900'
+    },
+    {
+        id: 9,
+        username: 'Sansa Stark',
+        profile: 'https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80',
+        email: 'sanstark@gmail.com',
+        status: 'active',
+        amount: 'Rp 949.000'
+    },
+    {
+        id: 10,
+        username: 'Samwell Tarly',
+        profile: 'https://images.unsplash.com/photo-1510951475896-ce8cacb1899f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        email: 'samtarly@gmail.com',
+        status: 'active',
+        amount: 'Rp 538.400'
+    },
+];
--- src/page/userList/UserList.jsx
+++ src/page/userList/UserList.jsx
@@ -1,10 +1,75 @@
-import React from 'react';
+import { useState } from 'react';
 import './UserList.css';
+import { DataGrid } from '@mui/x-data-grid';
+import { Delete } from '@material-ui/icons';
+import { UserRows } from '../../data';
+import { Link } from 'react-router-dom';
 
 const UserList = () => {
+  const [ data, setData ] = useState(UserRows);
+
+  const deleteHandler = (id) =>{
+    setData(data.filter((item)=>item.id !== id));
+  };
+
+  const columns = [{
+      field: 'id',
+      headerName: 'ID',
+      width: 70
+    },
+    {
+      field: 'user',
+      headerName: 'User name',
+      width: 200,
+      renderCell: (params) =>{
+        return (
+        <div className='user-table'>
+          <img className='user-img' src={params.row.profile} alt='user'/>
+          {params.row.username}
+        </div>
+        )
+      }
+    },
+    {
+      field: 'email',
+      headerName: 'E-mail',
+      width: 200
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: '100'
+    },
+    
+    {
+      field: 'edit',
+      headerName: 'Action',
+      width: 100,
+      renderCell: (params) =>{
+        return(
+          <>
+          <Link to={'/user/' + params.row.id}>
+          <button className='user-btn'>Edit</button>
+          </Link>
+          <Delete className='user-delete' 
+          onClick={()=> deleteHandler(params.row.id)}/>
+           </>
+        );
+      },
+    },
+  ];
   return (
-    <div className='userlist'>UserList</div>
-  )
+    <div className='userlist'>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[10]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+    </div>
+  );
 }
 
 export default UserList
